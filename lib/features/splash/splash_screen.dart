56f4aaa167384936be94397a60739f768e97cba8
Bhadresh7 <bhadreshpalani19@gmail.com>
2025-08-15 05:33:48 -0400

    initial commit

---
+++ lib/features/splash/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:farm_thoughts_web_app/core/constants/app_assets_constants.dart';
+import 'package:farm_thoughts_web_app/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3), () {
+      context.pushReplacementNamed("login");
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              AppAssetsConstants.logo,
+              width: 300,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
