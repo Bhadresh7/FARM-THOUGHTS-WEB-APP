99953eb6b62303c7a8e7b71055486d8f36bc3ab6
aseemahamed <kingman9407@gmail.com>
2025-08-16 15:15:24 +0530

    commit and ready to pull

--- lib/features/dashboard/screens/dashboard.dart
+++ lib/features/dashboard/screens/dashboard.dart
@@ -1,5 +1,4 @@
-import 'package:farm_thoughts_web_app/commons/widget/status_card.dart';
-import 'package:farm_thoughts_web_app/commons/widget/vendour_card.dart';
+
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -8,8 +7,6 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Vendour_Card(name:"aseem", profileImg: "https://playcontestofchampions.com/wp-content/uploads/2023/04/champion-iron-man.webp", address: "no16 haji hussain st", mobile: 93445634343, pricePerUnit: 100, timestamp: 20)
-
 
 
 
